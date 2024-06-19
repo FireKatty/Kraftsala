48d61802c423d31e7dc461e99b47ff2ecbe482b8
FireKatty <96065697+FireKatty@users.noreply.github.com>
2024-06-19 16:42:48 +0530

    Update themes.js

--- weather/src/components/themes.js
+++ weather/src/components/themes.js
@@ -1,27 +1,4 @@
 
-// export const lightTheme = {
-//   body: '#FFFFFF',
-//   text: '#000000',
-//   cardBackground: '#F3F3F3',
-//   buttonBackground: '#4caf50',
-//   buttonText: '#FFFFFF',
-//   searchBackground: '#FFFFFF',
-//   searchText: '#000000',
-//   border: 'none',
-// };
-
-// export const darkTheme = {
-//   body: '#333333',
-//   text: '#FFFFFF',
-//   cardBackground: '#444444',
-//   buttonBackground: '#555555',
-//   buttonText: '#FFFFFF',
-//   searchBackground: '#555555',
-//   searchText: '#FFFFFF',
-//   border: 'solid 2px #FFFFFF', // Define a solid border for dark theme
-// };
-
-
 export const lightTheme = {
   body: '#FFFFFF',              // Background color for the entire app
   text: '#000000',              // Text color
